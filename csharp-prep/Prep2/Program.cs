205ca8d1221edfde0d39653608375ceb5fa0f415
ProudNamwera4 <proudnamwera4@gmail.com>
2023-04-22 09:36:22 +0200

    computing letter grade and stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,66 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+
+        int grade = int.Parse(gradePercentage);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+           letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+
+
+        string sign = "";
+        int lastDigit = grade % 10;
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if(lastDigit < 3)
+        {
+            sign = "-";
+        }
+        else{
+            sign = "";
+        }
+
+        if (grade >= 97 || letter == "F")
+        {
+            sign = "";
+        }
+
+
+
+         Console.WriteLine($"Letter grade: {letter}{sign}");
+         if (letter == "F" || letter == "D")
+         {
+            Console.WriteLine("Unfortunately, you did not pass the course. Try again next time.");
+         }
+         else 
+         {
+            Console.WriteLine("Congratulations, you passed the course");
+         }
     }
 }
