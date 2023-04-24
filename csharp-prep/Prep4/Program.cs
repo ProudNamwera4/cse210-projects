7ddef4f4e9ecdb3f8a1bad4c971a629983920e4b
ProudNamwera4 <proudnamwera4@gmail.com>
2023-04-24 10:02:51 +0200

    practicing lists

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -5,5 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+
+        List<int> numbers = new List<int>();
+        int number = 0;
+
+        Console.WriteLine("Enter a lsit of numbers, type 0 when finished.");
+        do{
+            Console.Write("Enter a number: ");
+            string userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+
+            numbers.Add(number);
+        }while(number !=0 );
+        numbers.Remove(0);
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = number;
+        for (int i =0; i < numbers.Count; i++){
+            if (numbers[i] > largestNumber){
+                largestNumber = numbers[i];
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        int largestPositiveNumber =largestNumber;
+        for (int i =0; i< numbers.Count; i++){
+            if (numbers[i] < largestPositiveNumber && numbers[i] > 0){
+            largestPositiveNumber = numbers[i];
+            }
+        }
+        Console.WriteLine($"The largest positive number is: {largestPositiveNumber}");
+        
+        numbers.Sort();
+        foreach (int num in numbers){
+            Console.WriteLine("The sorted list is:");
+            Console.WriteLine(num);
+        }
     }
 }
