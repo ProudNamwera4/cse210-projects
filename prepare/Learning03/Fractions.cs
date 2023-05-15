d8edff8770789361278525d34c512b9fa3fa2fde
ProudNamwera4 <proudnamwera4@gmail.com>
2023-05-15 07:05:31 +0200

    Encapsulation learning activity : Fraction

---
+++ prepare/Learning03/Fractions.cs
@@ -0,0 +1,51 @@
+public class Fraction{
+    private int _top;
+    private int _bottom;
+
+    public Fraction(){
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top){
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom){
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public int GetTop(){
+        return _top;
+    }
+    public int SetTop(int top){
+        return _top = top;
+    }
+
+
+     public int GetBottom(){
+        return _bottom;
+    }
+    public int SetBottom(int bottom){
+        return _bottom = bottom;
+    }
+
+    public string GetFractionString(){
+
+        string devider = "/";
+        string top = _top.ToString();
+        string bottom = _bottom.ToString();
+
+        return top + devider + bottom;
+    }
+
+
+    public double GetDecimalValue(){
+        return (double)_top / (double)_bottom;
+
+    }
+
+
+}
