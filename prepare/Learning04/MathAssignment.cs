9a82081e4a8130928c919548e594f6df0219e202
ProudNamwera4 <proudnamwera4@gmail.com>
2023-06-01 15:26:59 +0200

    Inheritance Learning Activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,15 @@
+public class MathAssignment : Assignment{
+    private string _textbookSection;
+    private string _problems;
+    
+    public MathAssignment(string textbookSection, string problems, string name, string topic):base(name,topic){
+        _textbookSection = textbookSection;
+        _problems = problems;
+
+    }
+
+    public string GetHomeworkList(){
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+    
+        Assignment assignment = new Assignment("Proud", "Mathematics");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("7.3","8-19","Proud Namwera","Fractions");
+        Console.WriteLine($"{mathAssignment.GetSummary()} \n{mathAssignment.GetHomeworkList()}");
+
+        WritingAssignment writingAssignment = new WritingAssignment("Proud Namwera","European History","The Causes of World War II");
+        Console.WriteLine($"{writingAssignment.GetSummary()} \n{writingAssignment.GetWritingInformation()}");
+    
     }
 }
