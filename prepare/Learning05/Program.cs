61d910ab2428eb1933e1926192cf8a43558769ec
ProudNamwera4 <proudnamwera4@gmail.com>
2023-06-14 18:22:16 +0200

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red",4);
+        //Console.WriteLine($"{square.GetColor()} {square.GetArea()}");
+    
+        Rectangle rectangle = new Rectangle(4,2,"Blue");
+        //Console.WriteLine($"{rectangle.GetColor()} {rectangle.GetArea()}");
+    
+        Circle circle = new Circle(4,"Green");
+        //Console.WriteLine($"{circle.GetColor()} {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        shapes.Add(square);
+        
+        foreach(Shape shape in shapes){
+            Console.WriteLine($"{shape.GetColor()} {shape.GetArea()}");
+
+        }
+
     }
 }
